3ea63bd6a40ffdf6b4cc339a43d61bbe9a550eb6
Adina Singer <adinasinger@gmail.com>
2016-08-27 14:51:17 -0700

    pseudocode and skeleton code

---
+++ blender_app.py
@@ -0,0 +1,46 @@
+# menu: 0 - Main Menu, 1 - choose workout focus, 2 - choose exclusions, 3 - enter workout duration (minutes), 4 - enter new exercise
+
+def include_exercise(focus):
+	#show list of filter criteria (sep function?)
+	#prompt user to enter a workout focus from list
+	#return randomized list of exercises matching filter
+	pass
+
+def exclude_exercise(exclusion):
+	#show list of filter criteria (sep function?)
+	#prompt user to enter exclusion from list
+	#return randomized list of exercises matching filter
+	pass
+
+def workout_duration(minutes):
+	#default workout to 30 minutes
+	#prompt user for desired workout duration (in minutes)
+	#look up duration for entry in randomized exercise list
+	#use loop/conditional to add durations until greater or equal to arguement (move to next if over, until have checked all in list)
+	#return list of exercises 
+	pass
+
+def randomize_workout:
+	#shuffle returned list of exercises
+	#return shuffled list
+	pass
+
+def enter_excercise:
+	#prompt user for name of new exerise
+	#check for upper/lowercase
+	#if exercise name exists, warn user and return to prompt
+	#if exercise name doesn't exist, promt for details and then add to dictionary/database
+	#prompt for related data (see above) 
+	pass
+
+
+def main():
+#menu option 1
+#menu option 2
+#menu option 3
+#menu option 4
+#menu option 0
+pass
+
+if __name__ == '__main__':
+	main()
