2028e6d15e60fa6e205366813d3ebcba25e82068
Adina Singer <adinasinger@gmail.com>
2016-09-05 21:48:38 -0700

    updated menu and blender main fn

--- blender_app.py
+++ blender_app.py
@@ -1,6 +1,7 @@
 # menu: 0 - Main Menu, 1 - choose workout focus, 2 - choose exclusions, 3 - enter workout duration (minutes), 4 - enter new exercise
 
-#import class Exercise
+from class_Exercise import Exercise
+from blender_menu import print_menu
 
 
 def include_exercise(focus):
@@ -16,19 +17,26 @@ def exclude_exercise(exclusion):
 	pass
 
 def workout_duration(minutes):
-	#default workout to 15 minutes
+	#default workout to 15 minutes (any additional structure to circuits?)
 	#prompt user for desired workout duration (in minutes)
 	#look up duration for entry in randomized exercise list - OR can assume 30 sec duration ok for each set, and add or repeat exercises until duration met
 	#use loop/conditional to add durations until greater or equal to arguement (move to next if over, until have checked all in list)
 	#return list of exercises 
 	pass
 
-def randomize_workout:
+def workout_intensity(int):
+	#default workout to intensity 1 (any additional structure to circuits?)
+	#prompt user for desired workout intensity (1-4)
+	#look up difficulty level for entry in randomized exercise list - if not enough for set, ok to use lower intensities but NOT higher
+	#return list of exercises 
+	pass
+
+def randomize_workout():
 	#shuffle returned list of exercises
 	#return shuffled list
 	pass
 
-def enter_excercise:
+def add_excercise():
 	#prompt user for name of new exerise
 	#check for upper/lowercase
 	#if exercise name exists, warn user and return to prompt
@@ -36,14 +44,56 @@ def enter_excercise:
 	#prompt for related data (see above) 
 	pass
 
+def del_excercise():
+	#prompt user for name of exerise to delete
+	#check for upper/lowercase
+	#if exercise name does NOT exist, warn user and return to prompt
+	#if exercise name exists, promt to be sure if they want to delete
+	#if user sure they want to delete, then delete exercise from class list
+	pass
+
 
 def main():
-#menu option 1
-#menu option 2
-#menu option 3
-#menu option 4
-#menu option 0
-pass
+	exercise_list[]
+	Exercise("good mornings", int("1"), "upper body", "back", "standing")
+#file i/o to import rest of exercises?
+
+
+
+	print_menu()
+
+	menu_flag = True
+
+	while (menu_flag):
+		menu_input = int(raw_input("\nEnter Selection: "))
+		if menu_input == 0:
+			print_menu()
+		elif menu_input == 1:
+			print "Choose workout focus (type) from options"
+			#run include_focus()
+		elif menu_input == 2:
+			print "Choose body position from options"
+			#run exclude() OR just pick body position to filter list
+		elif menu_input == 3:
+			#define workout by duration and intensity; default workout length to 15 mins
+			print "Enter length of desired workout in minutes: "
+			print "Enter workout difficulty level: "
+		elif menu_input == 4:
+			#run add_exercise()
+			print "Add a new exercise (include name, difficulty level, type, muscle group worked, and body position) "
+		elif menu_input == 5:
+			print "Enter name of exercise to delete"
+			#run del_exercise()
+		elif menu_input == 9:
+			print "See you next workout!"
+			menu_flag = False
+		elif menu_input != "":
+			print "Please select a valid menu option"
+		else:
+			break
+	return menu_input
+
+
 
 if __name__ == '__main__':
 	main()
--- blender_menu.py
+++ blender_menu.py
@@ -1,5 +1,5 @@
 #menu for workour blender app
 
-def menu():
-	menu_input = int(raw_input(" 0 - Main Menu\n 1 - Choose workout focus(type)\n 2 - Choose body position\n 3 - Enter workout duration (minutes)\n 4 - Add new exercise\n 5 - Delete exercise\n 9 - Exit"))
-	return menu_input
+def print_menu():
+	print "\n 0 - Main Menu\n 1 - Filter workout focus(type)\n 2 - Filter body position\n 3 - Enter workout duration (minutes)\n 4 - Add new exercise\n 5 - Delete exercise\n 9 - Exit\n"
+
