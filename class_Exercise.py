927e6f2ce51bc3671b35c3f1343f4cc595fc9381
Grace <gracelee.durham@gmail.com>
2016-09-07 21:16:54 -0700

    include focus function v1

--- class_Exercise.py
+++ class_Exercise.py
@@ -2,7 +2,7 @@
 
 class Exercise(object):
 	def __init__(self, name, diff_level, exercise_type, muscle_group, body_position):
-		self.name = exercise_name
+		self.name = name
 		self.diff_level = diff_level
 		self.exercise_type = exercise_type
 		self.muscle_group = muscle_group
