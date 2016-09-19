6282a15e5cb42f10230294c528f16d17dfa844c4
Adina Singer <adinasinger@gmail.com>
2016-09-18 23:49:00 -0700

    build v2 from working simple app

--- blender_app_v2.py
+++ blender_app_v2.py
@@ -1,6 +1,6 @@
 
 from class_Exercise import Exercise
-from blender_menu import print_menu
+from blender_menu_v2 import print_menu
 from random import shuffle
 import csv
 
@@ -42,11 +42,21 @@ def exercise_counter(count_num, category):
 				count += 1
 			break
 
-def workout_generator():
-#generate random workout with 3 warm-up, 6 circuit, 2 cardio, 2 abs exercises
+
+def set_difficulty():
+	# get diff setting from user, return to variable to pass to generator
+	diff_input = int(raw_input("What level of difficulty would you like today?\nEnter choice (1-4): "))
+	return diff_input
+
+def set_circuit_count():
+# get number of circuits setting from user, return to variable to pass to generator
+	circuit_input = int(raw_input("How many circuits would you like to do today?\nEnter choice (1-3): "))
+	return circuit_input
+
+def workout_generator(diff, num1):
+#default generates random workout with 3 warm-up, 6 circuit, 2 cardio, 2 abs exercises at all diff levels
 	
 #while count <3, get name of exercise if type = warm-up (lowercase)
-	
 	print '\033[1m'"\nWarm-up"'\033[0m'
 	exercise_counter(3, "warm-up")
 	print ""
@@ -64,8 +74,11 @@ def workout_generator():
 	exercise_counter(2, "abs")
 	print ""
 	
+	print "Enter 0 for Main Menu or 5 to Exit"
 
 def main():
+	diff_choice = 4
+	circuit_choice = 1
 	#load list of exercises
 	import_exercise_list("Exercise_data.csv")
 	
@@ -74,41 +87,42 @@ def main():
 	#display menu
 	# menu: 0 - Main Menu, 1 - Generate workout, 2 - Change workout difficulty, 3 - Select number of circuits, 4 - Enter new exercise, 5 - Exit
 
-	# print_menu()
+	print_menu()
 
-	# menu_flag = True
+	menu_flag = True
 
-	# while (menu_flag):
-	# 	menu_input = int(raw_input("\nEnter Selection: "))
-	# 	if menu_input == 0:
-	# 		print_menu()
-	# 	elif menu_input == 1:
-	# 		print "Choose workout focus (type) from options"
-	# 		#run include_focus()
-	# 		focus_input = raw_input("What kind of workout do you want to focus on? ")
-	# 		include_exercise(focus_input)
-	# 	elif menu_input == 2:
-	# 		print "Choose body position from options"
-	# 		#run exclude() OR just pick body position to filter list
-	# 	elif menu_input == 3:
-	# 		#define workout by duration and intensity; default workout length to 15 mins
-	# 		print "Enter length of desired workout in minutes: "
-	# 		print "Enter workout difficulty level: "
-	# 	elif menu_input == 4:
-	# 		#run add_exercise()
-	# 		print "Add a new exercise (include name, difficulty level, type, muscle group worked, and body position) "
-	# 	elif menu_input == 5:
-	# 		print "See you next workout!"
-	# 		menu_flag = False
-	# 	elif menu_input != "":
-	# 		print "Please select a valid menu option"
-	# 	else:
-	# 		break
-	# return menu_input
+	while (menu_flag):
+		menu_input = int(raw_input("\nEnter menu selection: "))
+		if menu_input == 0:
+			print_menu()
+		elif menu_input == 1:
+			print "\nCurrent Settings - Difficulty: %i, # Circuits: %i" % (diff_choice, circuit_choice)
+			workout_generator(diff=diff_choice,num1=circuit_choice)
+		elif menu_input == 2:
+			print "\nCurrent Difficulty Setting: %i \n" % (diff_choice)
+			set_difficulty()
+			diff_choice = diff_input
+			print "\nNew Difficulty Setting: %i \n" % (diff_choice)
+		elif menu_input == 3:
+			print "\nCurrent Circuit Number Setting: %i \n" % (circuit_choice)
+			set_circuit_count()
+			circuit_choice = circuit_input
+			print "\nNew Circuit Number Setting: %i \n" % (circuit_choice)
+		elif menu_input == 4:
+			print "Add a new exercise (include name, type, difficulty level, muscle group worked, and body position) "
+			#run add_exercise()
+		elif menu_input == 5:
+			print "\nSee you next workout!\n"
+			menu_flag = False
+		elif menu_input != "":
+			print "Please select a valid menu option"
+		else:
+			break
+	return menu_input
 
 	#generate random workout with 2 warm-up, 4 circuit, 2 cardio, 2 abs exercises
-	workout_generator()
-
+	#workout_generator(diff=diff_choice,num1=circuit_choice)
+#
 
 
 if __name__ == '__main__':
---
+++ blender_menu_v2.py
@@ -0,0 +1,6 @@
+#menu for workout blender app v2
+
+def print_menu():
+	# menu: 0 - Main Menu, 1 - Generate workout, 2 - Change workout difficulty, 3 - Select number of circuits, 4 - Enter new exercise, 5 - Exit
+	print "\n 0 - Main Menu\n 1 - Generate workout\n 2 - Change workout difficulty\n 3 - Select number of circuits\n 4 - Enter new exercise\n 5 - Exit\n"
+
